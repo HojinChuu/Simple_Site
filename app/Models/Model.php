80892c3752a74d2a3f863caedc38a67180827c43
HojinChuu <cnghwls7@gmail.com>
2020-08-04 15:23:32 +0900

    instanciate post model

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Database\DBConnection;
-use stdClass;
 
 abstract class Model
 {
@@ -17,13 +16,15 @@ abstract class Model
     public function all() : array
     {
         $stmt = $this->db->getPDO()->query("SELECT * FROM {$this->table} ORDER BY created_at DESC");
-        
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, get_class($this), array($this->db));
+
         return $stmt->fetchAll();
     }
 
-    public function findById(int $id) : stdClass
+    public function findById(int $id) : Model
     {
         $stmt = $this->db->getPDO()->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+        $stmt->setFetchMode(\PDO::FETCH_CLASS, get_class($this), array($this->db));
         $stmt->execute([$id]);
 
         return $stmt->fetch();
--- views/blog/index.php
+++ views/blog/index.php
@@ -1,6 +1,7 @@
 <h1>News</h1>
 
 <?php foreach($params['posts'] as $post) : ?>
+    <?php var_dump($post) ?>
     <div class="card mb-3">
         <div class="card-body">
             <h2><?= $post->title ?></h2>
